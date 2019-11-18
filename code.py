477d1d17f5e3d271cecfb47b60c643ad1b43001b
premtodkar <premtodkar@users.noreply.github.com>
2019-11-18 17:13:05 +0530

    updated file

--- code.py
+++ code.py
@@ -3,8 +3,9 @@ import requests
 r = requests.get('https://jsonplaceholder.typicode.com/comments?postId=1')
 json_response = r.json()
 #print (json_response)
+
+#For processing last lines
 a=json_response[len(json_response)-2]
-#print(a)
 b=json_response[len(json_response)-1]
 lista = [k for k in a.values()]
 listb = [k for k in b.values()]
@@ -15,7 +16,7 @@ db=print(listb[1])
 
 
 #Net Calc
-
+data=1
 for k in json_response:
     prem=-1
     listf = [m for m in k]
@@ -24,4 +25,7 @@ for k in json_response:
         prem+=1
         if(j== 'email'):
             listq = [k for k in k.values()]
-            print(listq[prem])
+            data1=listq[prem-2]
+            proc=((data1-data)/data)*100
+            data=data1
+            print(proc)
